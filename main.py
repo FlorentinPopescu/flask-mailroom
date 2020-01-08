4cc6860d95d27ba200ab15cdafb1a003c844464d
fpopescu <popescu01@gmail.com>
2020-01-08 13:00:51 -0600

    commit_08

--- main.py
+++ main.py
@@ -15,13 +15,34 @@ app.secret_key = os.environ.get('SECRET_KEY').encode()
 @app.route('/')
 def home():
     return redirect(url_for('all'))
-
 # ---------------------------------
+
 @app.route('/donations/')
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+# ---------------------------------
+
+@app.route('/donations/select/', methods=["GET", "POST"])
+def select():
+    if request.method == "GET":
+        donations = Donation.select()
+        try:
+            don = request.args.get('selected_donor', None)
+            if don is None:
+                return render_template('select.jinja2')
+            try:
+                s_donor = Donor.get(Donor.name == don)
+                selected_donor = [(donation.donor.name, donation.value) for donation in donations \
+                        if don is not None and donation.donor.name == don]
+                total_unique = len(selected_donor)
+            except Donor.DoesNotExist:
+                return render_template('select.jinja2', error="reenter donor's name")
+            return render_template("unique.jinja2", selection=selected_donor, text=s_donor.name, counts=total_unique)
+        except ValueError:
+            return render_template("select.jinja2")
+# ---------------------------------
+
 # ---------------------------------
 if __name__ == "__main__":
     app.run(debug=True)
